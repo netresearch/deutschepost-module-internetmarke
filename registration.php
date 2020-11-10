51503d2b60e047973ef1c37887548b75cef59eee
Christoph Aßmann <christoph.assmann@netresearch.de>
2020-11-10 17:16:19 +0100

    DHLGW-1003: initial commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DeutschePost_Internetmarke',
+    __DIR__
+);
