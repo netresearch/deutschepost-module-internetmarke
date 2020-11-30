80a063e97566d90d1ebddf630e6e9872f0cf0caa
Christoph Aßmann <christoph.assmann@netresearch.de>
2020-11-30 12:43:35 +0100

    DHLGW-1007: configure page format

---
+++ Model/Config/Source/PageFormats.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace DeutschePost\Internetmarke\Model\Config\Source;
+
+use DeutschePost\Internetmarke\Api\Data\PageFormatInterface;
+use DeutschePost\Internetmarke\Model\ResourceModel\PageFormatCollection;
+use DeutschePost\Internetmarke\Model\ResourceModel\PageFormatCollectionFactory;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class PageFormats implements OptionSourceInterface
+{
+    /**
+     * @var PageFormatCollectionFactory
+     */
+    private $collectionFactory;
+
+    public function __construct(PageFormatCollectionFactory $collectionFactory)
+    {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function getCollection(): PageFormatCollection
+    {
+        $collection = $this->collectionFactory->create();
+
+        // only allow single-item page formats
+        $collection->addFieldToFilter(PageFormatInterface::VOUCHER_COLUMNS, ['eq' => 1]);
+        $collection->addFieldToFilter(PageFormatInterface::VOUCHER_ROWS, ['eq' => 1]);
+
+        return $collection;
+    }
+
+    public function toOptionArray()
+    {
+        $emptyOption = [
+            'value' => '',
+            'label' => __('-- Please Select --'),
+        ];
+
+        /** @var PageFormatInterface[] $pageFormats */
+        $pageFormats = $this->getCollection()->getItems();
+        if (empty($pageFormats)) {
+            return [$emptyOption];
+        }
+
+        $options = [
+            $emptyOption,
+            'standard' => [
+                'label' => __('Standard Formats'),
+                'value' => [],
+            ],
+            'brother' => [
+                'label' => 'Brother',
+                'value' => [],
+            ],
+            'dymo' => [
+                'label' => 'Dymo',
+                'value' => [],
+            ],
+            'herma' => [
+                'label' => 'Herma',
+                'value' => [],
+            ],
+            'leitz' => [
+                'label' => 'Leitz',
+                'value' => [],
+            ],
+            'seiko' => [
+                'label' => 'Seiko',
+                'value' => [],
+            ],
+            'zweckform' => [
+                'label' => 'Zweckform',
+                'value' => [],
+            ],
+        ];
+
+        $companyNames = array_keys($options);
+
+        foreach ($pageFormats as $pageFormat) {
+            $company = strstr(strtolower($pageFormat->getName()), ' ', true);
+
+            if (!in_array($company, $companyNames, true)) {
+                $company = 'standard';
+            }
+
+            $options[$company]['value'][]= [
+                'value' => $pageFormat->getId(),
+                'label' => sprintf(
+                    "%s%s",
+                    $pageFormat->getName(),
+                    $pageFormat->isAddressPossible() ? ' (A)' : ''
+                ),
+            ];
+        }
+
+        return $options;
+    }
+}
