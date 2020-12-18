74db1989ce9ded0175b9f4e5d19ca35dd629c989
Sebastian Ertner <sebastian.ertner@netresearch.de>
2020-12-18 11:14:00 +0000

    DHLGW-1037: modify tracking request for Deutsche Post tracking numbers

---
+++ Plugin/Pipeline/SendRequestStagePlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace DeutschePost\Internetmarke\Plugin\Pipeline;
+
+use DeutschePost\Internetmarke\Model\Tracking\TrackingConfiguration;
+use Dhl\ShippingCore\Api\Data\Pipeline\TrackRequest\TrackRequestInterface;
+use Dhl\UnifiedTracking\Model\Pipeline\Stage\SendRequestStage;
+
+class SendRequestStagePlugin
+{
+    /**
+     * @param SendRequestStage $subject
+     * @param TrackRequestInterface[] $requests
+     *
+     * @return null
+     */
+    public function beforeExecute(SendRequestStage $subject, array $requests)
+    {
+        foreach ($requests as $request) {
+            $trackExtensionAttributes = $request->getSalesTrack()->getExtensionAttributes();
+            if ($trackExtensionAttributes->getDpdhlLabelApi()) {
+                $request->getSalesTrack()->setCarrierCode(TrackingConfiguration::CARRIER_CODE);
+            }
+        }
+
+        return null;
+    }
+}
