40ef903a0e49e919e01dd39862c4e46fc0b796fd
Christoph Aßmann <christoph.assmann@netresearch.de>
2022-03-16 13:27:23 +0100

    DHLGW-1191: add config validation checks

---
+++ Model/Config/ItemValidator/Group.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace DeutschePost\Internetmarke\Model\Config\ItemValidator;
+
+class Group
+{
+    public const CODE = 'dpim';
+}
