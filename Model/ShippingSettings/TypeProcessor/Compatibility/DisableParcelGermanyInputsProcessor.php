a6100df5b8fad64015830e544949d3ac06f9bc5f
Christoph Aßmann <christoph.assmann@netresearch.de>
2023-05-02 15:47:05 +0200

    DHLGW-1248: disable pddp service if internetmarke product gets selected

--- Model/ShippingSettings/TypeProcessor/Compatibility/DisableParcelGermanyInputsProcessor.php
+++ Model/ShippingSettings/TypeProcessor/Compatibility/DisableParcelGermanyInputsProcessor.php
@@ -93,6 +93,7 @@ class DisableParcelGermanyInputsProcessor implements CompatibilityProcessorInter
             Codes::SERVICE_OPTION_PRINT_ONLY_IF_CODEABLE,
             Codes::SERVICE_OPTION_PREMIUM,
             Codes::SERVICE_OPTION_NO_NEIGHBOR_DELIVERY,
+            Codes::SERVICE_OPTION_PDDP,
             // some inputs of the "package details" package option
             sprintf('%s.%s', CoreCodes::PACKAGE_OPTION_DETAILS, CoreCodes::PACKAGE_INPUT_WIDTH),
             sprintf('%s.%s', CoreCodes::PACKAGE_OPTION_DETAILS, CoreCodes::PACKAGE_INPUT_LENGTH),
